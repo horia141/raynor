931f797d44aba8ec07741bb332fb7f6751d39aab
Horia Coman <horia141@gmail.com>
2018-05-05 10:39:20 +0200

    Make the path marshaller accept _ in paths

--- src/path.ts
+++ src/path.ts
@@ -3,13 +3,13 @@ import { StringMarshaller } from './string'
 
 
 export class AbsolutePathMarshaller extends StringMarshaller {
-    private static readonly _pathRegExp: RegExp = new RegExp('^[/]([a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?[/]?)*$');
+    private static readonly _pathRegExp: RegExp = new RegExp('^[/]([a-zA-Z0-9_]([a-zA-Z0-9_-]*[a-zA-Z0-9_])?[/]?)*$');
 
     filter(a: string): string {
-	      if (!AbsolutePathMarshaller._pathRegExp.test(a)) {
-	          throw new ExtractError('Expected an absolute path');
-	      }
+        if (!AbsolutePathMarshaller._pathRegExp.test(a)) {
+            throw new ExtractError('Expected an absolute path');
+        }
 
-	      return a;
+        return a;
     }
 }
