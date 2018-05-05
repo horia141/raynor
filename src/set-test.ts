cb68582edf270018904bfaf8683b2f7df4f06e92
Horia Coman <horia141@gmail.com>
2018-05-05 11:15:07 +0200

    Add a set marshaller

---
+++ src/set-test.ts
@@ -0,0 +1,93 @@
+import { expect } from 'chai'
+import 'mocha'
+
+import { IntegerMarshaller } from './number'
+import { SetMarshaller } from './set'
+
+
+describe('SetMarshaller', () => {
+    const integerMarshaller = new IntegerMarshaller();
+
+    const Sets = [
+        {
+            raw: [],
+            built: new Set<number>([])
+        },
+        {
+            raw: [10, 20],
+            built: new Set<number>([10, 20])
+        }
+    ];
+
+    const BadSets = [
+        {
+            raw: [10.2, 20.3],
+            built: new Set<number>([10.2, 20.3])
+        },
+        {
+            raw: ['hello', 'world'],
+            built: new Set<string>(['hello', 'world'])
+        }
+    ];
+
+    const NonSets = [
+        10,
+        31.23,
+        null,
+        undefined,
+        NaN,
+        Number.POSITIVE_INFINITY,
+        Number.NEGATIVE_INFINITY,
+        true,
+        false,
+        'hello',
+        '100',
+    ];
+
+    describe('extract', () => {
+        for (const { raw, built } of Sets) {
+            it(`should extract ${JSON.stringify(raw)}`, () => {
+                const setMarshaller = new SetMarshaller(integerMarshaller);
+                expect(setMarshaller.extract(raw)).to.eql(built);
+            })
+        }
+
+        for (const badCase of BadSets) {
+            it(`should throw for ${JSON.stringify(badCase)}`, () => {
+                const setMarshaller = new SetMarshaller(integerMarshaller);
+
+                expect(() => setMarshaller.extract(badCase)).to.throw;
+            });
+        }
+
+        for (const setMap of NonSets) {
+            it(`should throw for ${JSON.stringify(setMap)}`, () => {
+                const setMarshaller = new SetMarshaller(integerMarshaller);
+
+                expect(() => setMarshaller.extract(setMap)).to.throw('Expected an array');
+            });
+        }
+    });
+
+    describe('pack', () => {
+        for (const { raw, built } of Sets) {
+            it(`should produce the same input for ${JSON.stringify(raw)}`, () => {
+                const setMarshaller = new SetMarshaller(integerMarshaller);
+                expect(setMarshaller.pack(built)).to.eql(raw);
+            });
+        }
+    });
+
+    describe('extract and pack', () => {
+        for (const { raw } of Sets) {
+            it(`should be opposites for ${JSON.stringify(raw)}`, () => {
+                const setMarshaller = new SetMarshaller(integerMarshaller);
+
+                const extracted = setMarshaller.extract(raw);
+                const packed = setMarshaller.pack(extracted);
+
+                expect(packed).to.eql(raw);
+            });
+        }
+    });
+});
---
+++ src/set.ts
@@ -0,0 +1,32 @@
+import { Marshaller, ExtractError } from './core'
+
+
+export class SetMarshaller<V> implements Marshaller<Set<V>> {
+    private readonly _valueMarshaller: Marshaller<V>;
+
+    constructor(valueMarshaller: Marshaller<V>) {
+        this._valueMarshaller = valueMarshaller;
+    }
+
+    extract(raw: any): Set<V> {
+        const result = new Set<V>([]);
+
+        if (!Array.isArray(raw)) {
+            throw new ExtractError('Expected an array');
+        }
+
+        for (const elem of raw) {
+            result.add(this._valueMarshaller.extract(elem));
+        }
+
+        return result;
+    }
+
+    pack(set: Set<V>): any {
+        const res = [];
+        for (const elem of set.values()) {
+            res.push(this._valueMarshaller.pack(elem));
+        }
+        return res;
+    }
+}
